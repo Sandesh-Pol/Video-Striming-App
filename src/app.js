6b9e6733bf7a6b74dc721138a145a2d23e94fce9
Sandesh-Pol <sandeshpol268@example.com>
2025-01-01 23:34:50 +0530

    Fist route created

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from './routes/user.routes.js';
 
-const app =  express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORE_ORIGIN,
-    credentials:true
-}))
+    credentials: true,
+}));
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app} 
+// Routes
+app.use("/api/v1/users", userRouter);
+
+export { app };
